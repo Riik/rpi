a66e9961bfebda82c2680f5e29b343f1be94f3dd
Riik <rikvandervlist@gmail.com>
2014-07-28 23:30:20 +0200

    Title is split in two lines max 16 chars each

--- news.py
+++ news.py
@@ -1,7 +1,20 @@
 import feedparser
 url = 'http://feeds.nos.nl/nosjournaal?format=xml'
 feed = feedparser.parse(url)
-for item in feed["items"]:
-    x = item["title"]
-    x = x.encode('ascii','replace')
-    print(len(x))
+#for item in feed["items"]:
+#    title = item["title"].encode('ascii','replace')
+#    print(len(title))
+title = feed["items"][0]["title"].encode('ascii','replace')
+print(title)
+# split title in two parts with max length 16, without breaking words
+i = 16
+while title[i] != ' ':
+    i = i - 1
+k = i + 16
+while title[k] != ' ':
+    k= k - 1
+t = []
+t.append(title[:i])
+t.append(title[i:k])
+print(t)
+    
