2e515031fa91d2e56920cb7e4e1090f6c5838eea
Riik <rikvandervlist@gmail.com>
2014-07-28 23:36:37 +0200

    Rewrote into function to get news and function to display news

--- news.py
+++ news.py
@@ -1,20 +1,21 @@
 import feedparser
-url = 'http://feeds.nos.nl/nosjournaal?format=xml'
-feed = feedparser.parse(url)
-#for item in feed["items"]:
-#    title = item["title"].encode('ascii','replace')
-#    print(len(title))
-title = feed["items"][0]["title"].encode('ascii','replace')
-print(title)
-# split title in two parts with max length 16, without breaking words
-i = 16
-while title[i] != ' ':
-    i = i - 1
-k = i + 16
-while title[k] != ' ':
-    k= k - 1
-t = []
-t.append(title[:i])
-t.append(title[i:k])
-print(t)
+def get_news():
+    url = 'http://feeds.nos.nl/nosjournaal?format=xml'
+    feed = feedparser.parse(url)
+    #for item in feed["items"]:
+    #    title = item["title"].encode('ascii','replace')
+    #    print(len(title))
+    title = feed["items"][0]["title"].encode('ascii','replace')
+    print(title)
+    # split title in two parts with max length 16, without breaking words
+    i = 16
+    while title[i] != ' ':
+        i = i - 1
+    k = i + 16
+    while title[k] != ' ':
+        k= k - 1
+    t = []
+    t.append(title[:i])
+    t.append(title[i:k])
+    return t
     
