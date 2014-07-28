abc300234b302c40605660db3afb09b581b5a2d7
Riik <rikvandervlist@gmail.com>
2014-07-28 22:00:04 +0200

    .

--- test.py
+++ test.py
@@ -5,11 +5,17 @@ from temp import get_temperature
 i = 0;
 refreshtime = 30
 temperature = get_temperature()
+# clear screen
+os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 10:0')
+# set contrast 
+os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 12:000f')
+# set brightness
+os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 13:0080')
 while 1:
 	# get system time in specified format
 	timestr = time.strftime("%H:%M:%S")
-	# clear screen
-	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 10:0')
+	# reset cursor
+        os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 11:00')
 	# print time
 	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -t {0}'.format(timestr))
 	# set cursor to new line
