845fdaf15677aacbeaa37afca4d3142450ab029b
Riik <rikvandervlist@gmail.com>
2014-07-31 15:50:13 +0200

    Changed the daemon

--- news_daemon.py
+++ news_daemon.py
@@ -1,6 +1,7 @@
-import daemon
-
 from disp_news import news_displayer
 
-with daemon.DeamonContext():
-    news_displayer()
+from daemon import runner
+
+app = news_displayer()
+daemon_runner = runner.DaemonRunner(app)
+daemon_runner.do_action()
