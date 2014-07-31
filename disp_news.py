b29acd12da6259bcf09bc430db53d835d0a5c00d
Riik <rikvandervlist@gmail.com>
2014-07-31 15:25:45 +0200

    'aha'

--- disp_news.py
+++ disp_news.py
@@ -2,26 +2,27 @@ import time
 import os
 # import function that gets temperature data from other file
 from news import get_news
-i = 0;
-refreshtime = 60
-# clear screen
-os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 10:0')
-# set contrast 
-os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 12:000f')
-# set brightness
-os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 13:0080')
-while 1:
-	t = get_news()
-	print(t[0])
-	print(t[1])
-	# clear screen
-	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 10:0')
-	# write first line
-	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -t {}'.format(t[0]))
-	# set cursor to new line
-	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 11:20')
-	# print weather data
-	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -t {}'.format(t[1]))
-	time.sleep(refreshtime)
+def news_displayer():
+    i = 0;
+    refreshtime = 60
+    # clear screen
+    os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 10:0')
+    # set contrast 
+    os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 12:000f')
+    # set brightness
+    os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 13:0080')
+    while 1:
+   	t = get_news()
+   	print(t[0])
+   	print(t[1])
+   	# clear screen
+   	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 10:0')
+   	# write first line
+   	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -t {}'.format(t[0]))
+   	# set cursor to new line
+   	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -w 11:20')
+   	# print weather data
+   	os.system('bw_tool -I -a 94 -D /dev/i2c-1 -t {}'.format(t[1]))
+   	time.sleep(refreshtime)
 
-
+news_displayer()
