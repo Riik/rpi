ff1ca9b8ffa78cbc04fc0a456b05b8db63abaf16
Riik <rikvandervlist@gmail.com>
2014-10-26 21:52:52 +0100

    .

---
+++ plysm.py
@@ -0,0 +1,18 @@
+import glob
+import random
+import time
+import pygame
+
+x = glob.glob('./wavjes/*.wav')
+print("Number of files found in /wavjes:")
+print(len(x))
+
+random.seed(time.time())
+s = []
+for filename in x:
+    s.append(pygame.mixer.Sound(filename))
+    print("loaded: {}".format(filename))
+for i in xrange(5):
+    rand = random.randint(0, len(s))
+    s[rand].play()
+
