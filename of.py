63d89e2b435f9a2761348d5aad9571531a77676a
Riik <rikvandervlist@gmail.com>
2014-10-26 22:13:48 +0100

    of.py test

--- of.py
+++ of.py
@@ -1,31 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 import pygame
+import random
+import glob
+
 
 def buttonEvent (pin):
 	global s
-	print("callback called")
-	x = 0
-	time.sleep(.050)
-	for i in xrange(5):
-		x += GPIO.input(18)	
-	print(x)
-	if x > 2:
-		print("Fridge opened")
-		s.play()
+	print(GPIO.input(18))
+	if(GPIO.input(18)):
+		print("Falling edge detected")
+		rand = random.randint(0, len(s))
+                s[rand].play()
 
 def main():
 	global s
 	pygame.mixer.init()
-	s = pygame.mixer.Sound("Explosion.wav")
-	s.play()	
+	random.seed(time.time())
+        pygame.mixer.init()
+
+        s = []
+        for filename in x:
+            s.append(pygame.mixer.Sound(filename))
+            print("loaded: {}".format(filename))
+	
 	GPIO.setmode(GPIO.BCM)
 
 	GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-	GPIO.add_event_detect(18, GPIO.FALLING, callback=buttonEvent, bouncetime=200)
+	GPIO.add_event_detect(18, GPIO.FALLING, callback=buttonEvent, bouncetime=150)
 	while True:
-		time.sleep(.50)
-
+		time.sleep(150)
+		print("Running...")
 if __name__=="__main__":
 	main()
