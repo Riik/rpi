26d544ef6032a0c6068dce6a402b64c58938523b
Riik <rikvandervlist@gmail.com>
2014-10-26 20:46:28 +0100

    .

--- iotest.py
+++ iotest.py
@@ -1,25 +1,16 @@
 import RPi.GPIO as GPIO
 import time
-import pygame
-
-def buttonEvent (pin):
-	global s
-	print("Falling edge detected")
-	s.play()
 
 def main():
-	global s
+
 	pygame.mixer.init()
-	s = pygame.mixer.Sound("Explosion.wav")
 	
 	GPIO.setmode(GPIO.BCM)
 
 	GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-	GPIO.add_event_detect(18, GPIO.FALLING, bouncetime=300)
-	GPIO.add_event_callback(18,buttonEvent)
 	while True:
-		time.sleep(150)
+		time.sleep(50)
+		print(GPIO.input(18)
 		print("Running...")
 if __name__=="__main__":
 	main()
